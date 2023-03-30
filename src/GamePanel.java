d99e31258fa3ff36b19474520e756bb6cc599ca1
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2023-03-30 22:24:24 +0530

    Header image added. Rectangle drawn.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,24 @@
+import java.awt.*;
+
+//import java.awt.Color;
+
+//import java.awt.Graphics;
+
+
 import javax.swing.*;
 public class GamePanel extends JPanel{
-    
+    private ImageIcon snaketitile = new ImageIcon(getClass().getResource("snaketitle.jpg"));    
+
+    GamePanel(){
+        
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        g.setColor(Color.white);
+        g.drawRect(24,10,851,55); 
+        g.drawRect(24,74,851,576); 
+        snaketitile.paintIcon(this,g,25,11);
+        g.setColor(Color.BLACK); 
+        g.fillRect(25,75,850,575);
+    }
 }
